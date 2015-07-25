bf7f59e4ce12772036add626069c0bcbf9438a6f
Justin Crown <jcrown@mediatemple.net>
2015-07-25 00:03:39 -0700

    Switching to PyMySQL

--- har_api/settings.py
+++ har_api/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if os.environ.get('TRAVIS'):
-    SQLALCHEMY_DATABASE_URI = 'mysql://travis:@localhost/haralyzer_api_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymsql://travis:@localhost/haralyzer_api_test'
 else:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(
         os.path.join(BASE_DIR, 'data-dev.db'))
