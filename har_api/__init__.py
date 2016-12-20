414d9840bb05e3027f00699a47ae85f8a46da2b8
Justin Crown <jcrown@mediatemple.net>
2016-12-20 10:47:36 -0800

    Adding permissive CORS

--- har_api/__init__.py
+++ har_api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask.ext.restful import Api
 from har_api.models import db
 from har_api.routes import create_routes
@@ -6,6 +7,7 @@ from har_api.routes import create_routes
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     api = Api(app)
 
     app.config.from_object('har_api.settings')
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import sys
 
 install_requires = [
     "flask",
+    "flask-cors",
     "Flask-Migrate",
     "Flask-RESTful",
     "Flask-SQLAlchemy",
