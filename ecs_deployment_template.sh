d38519f756f99090ea6d78619ed962af6d320578
Justin Crown <justin.crown@spinifexgroup.com>
2017-01-30 10:12:38 -0800

    adding mysql/redis to dev push

--- ecs_deployment_template.sh
+++ ecs_deployment_template.sh
@@ -16,10 +16,30 @@ cat > workflows-demo-task-definition.json <<EOF
       "mountPoints": [],
       "name": "todo-demo",
       "environment": [],
-      "links": [],
+      "links": ["mysql", "redis"],
       "image": "$AWS_REGISTRY_URL/testing2:debug-$WERCKER_GIT_BRANCH",
       "command": ["/usr/local/bin/python", "/pipeline/source/manage.py", "runserver", "-h", "0.0.0.0"],
       "cpu": 10
+    },
+    {
+      "environment": [
+        {
+          "name": "MYSQL_ROOT_PASSWORD",
+          "value": "testpass"
+        }
+      ],
+      "name": "mysql",
+      "image": "mysql",
+      "cpu": 10,
+      "memory": 500,
+      "essential": true
+    },
+    {
+      "name": "redis",
+      "image": "redis",
+      "cpu": 10,
+      "memory": 500,
+      "essential": true
     }
 ]
 EOF
